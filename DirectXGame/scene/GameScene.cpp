d8e91fccf3e0d67854fc34a19e05e231a68b6ea6
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-04-18 13:09:12 +0900

    01_03資料P29までの機能実装

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,19 +1,55 @@
 #include "GameScene.h"
+#include "ImGuiManager.h"
 #include "TextureManager.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	txHandle_ = TextureManager::Load("sample.png"); // テクスチャを読み込む
+	sprite_ = Sprite::Create(txHandle_, {100, 50}); // スプライトの生成
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	soundDataHandle_ = audio_->LoadWave("fanfare.wav");      // サウンドデータの読みこみ
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true); // 音声再生
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	Vector2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	sprite_->SetPosition(position);
+
+	if (input_->TriggerKey(DIK_SPACE)) {
+		if (audio_->IsPlaying(voiceHandle_)) {
+			// 音声停止
+			audio_->StopWave(voiceHandle_);
+		} else {
+			voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+		}
+	}
+
+#ifdef _DEBUG
+	// デバッグモードでのみ使用可能
+	ImGui::Begin("Debug1");
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::SliderFloat3("sliderFloat3", inputFloat3, 0.0f, 1.0f);
+	// ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31); // デバッグテキストの表示
+	ImGui::End();
+	ImGui::ShowDemoWindow();
+#endif
+}
 
 void GameScene::Draw() {
 
@@ -40,6 +76,7 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	model_->Draw(worldTransform_, viewProjection_, txHandle_);
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
@@ -52,6 +89,7 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
+	sprite_->Draw();
 	/// </summary>
 
 	// スプライト描画後処理
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -2,6 +2,7 @@
 
 #include "Audio.h"
 #include "DirectXCommon.h"
+#include "ImGuiManager.h"
 #include "Input.h"
 #include "Model.h"
 #include "Sprite.h"
@@ -43,6 +44,14 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+	uint32_t txHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+	uint32_t soundDataHandle_ = 0; // サウンドデータハンドル
+	uint32_t voiceHandle_ = 0;     // 音声再生ハンドル
+	float inputFloat3[3] = {0, 0, 0};//inputで値を入力する関数
 
 	/// <summary>
 	/// ゲームシーン用
