8b009a04ed8e655c71a9dd78e607d992bc84fb5d
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-07-22 15:23:13 +0900

    重力加速度微調整

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -80,9 +80,9 @@ private:
 	float turnTimer_ = 0.0f;                                // 旋回タイマー
 	static inline const float kTimeTurn = 0.3f;             // 旋回時間<秒>
 	bool onGround_ = true;                                  // 設置状態フラグ
-	static inline const float kGravityAcceleration = 0.05f; // 重力加速度(下方向)
+	static inline const float kGravityAcceleration = 0.04f; // 重力加速度(下方向)
 	static inline const float kLimitFallSpeed = 0.4f;       // 最大落下速度(下方向)
-	static inline const float kJumpAcceleration = 0.6f;    // ジャンプ初速(上方向）
+	static inline const float kJumpAcceleration = 0.6f;     // ジャンプ初速(上方向）
 	static inline const float kGroundPos = 2.0f;            // 地面の座標
 	bool landing = false;
 	static inline const float kWidth = 1.8f; // キャラクターの当たり判定サイズ
